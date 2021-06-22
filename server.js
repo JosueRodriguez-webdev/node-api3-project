a03c32100386b593ce324f91cbd32cdaae0d75df
Josue Rodriguez <maybejosue@gmail.com>
2021-06-22 16:41:20 -0700

    Completes middleware function and creates routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+//Routes
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,14 @@ server.get("/", (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now());
+  next();
+}
+
+//
+
+server.use("/post", logger, postRouter);
+server.use("/user", logger, userRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,70 @@
-const express = require('express');
+const express = require("express");
+const helper = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const ID = req.params.id;
+
+  helper
+    .getById(ID)
+    .then((r) => {
+      if (!r) {
+        return res.status(400).json({ message: "invalid user id" });
+      }
+      req.user = r;
+      next();
+    })
+    .catch((e) => {
+      return res.status(400).json({ message: "missing required name field" });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body || Object.keys(req.body) == 0) {
+    return res.status(400).json({ message: "missing user data" });
+  } else if (!req.body.name) {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body || Object.keys(req.body) == 0) {
+    return res.status(400).json({ message: "missing post data" });
+  } else if (!req.body.text) {
+    return res.status(400).json({ message: "missing required text field" });
+  }
+  next();
 }
 
 module.exports = router;
